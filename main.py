831175e8e84ad9b454bfc796679b7b1e723ddb40
banikrukov <danyalapin@yandex.ru>
2022-09-02 22:33:41 +0500

    [MODIFY] correctly get data from the website

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from config import token
+from scraping.mvideo_parser import MvideoParser
 
 bot = telebot.TeleBot(token=token)
 
@@ -7,5 +8,7 @@ bot = telebot.TeleBot(token=token)
 def start_message(message):
     bot.send_message(message.chat.id, 'Hello, i am a price bot')
 
-
-bot.infinity_polling()
+if __name__ == '__main__':
+    # bot.infinity_polling()
+    instance = MvideoParser()
+    print(instance.find_price("iphone 13"))
--- scraping/mvideo_parser.py
+++ scraping/mvideo_parser.py
@@ -1,17 +1,44 @@
-from parser import Parser
-from selenium import webdriver
+from .parser import Parser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from time import sleep
+from fake_useragent import UserAgent
+from selenium.webdriver.common.by import By
+
 
 class MvideoParser(Parser):
 
     #Overriding abstract method
     def find_price(self, product_name: str) -> int:
-        url = "https://www.mvideo.ru/product-list-page?q=" + product_name.replace(' ', '+')
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get(url)
-        content = driver.page_source
-        soup = BeautifulSoup(content)
+        content = self.get_page_content(product_name=product_name)
+        soup = BeautifulSoup(content, features="lxml")   
         
-        return soup.findAll('span', attrs={'class':"price__main-value"})
+        return soup.findAll('div', attrs={'class':"_3NaXx _33ZFz _2m5MZ"})
 
+
+    def get_page_content(self, product_name:str) -> str:
+        ua = UserAgent()
+        options = Options()
+        agent = ua.random
+        options.add_argument("window-size=1400,600")
+        options.add_argument(f'user-agent={agent}')
+
+        url = "https://market.yandex.ru/"
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        
+        driver.get(url)
+        sleep(20)
+        search_element = driver.find_element(By.XPATH, '//*[@id="header-search"]')
+        search_element.click()
+        sleep(2)
+        search_element.clear()
+        sleep(2)
+        search_element.send_keys(product_name)
+        sleep(2)
+        search_element.send_keys(Keys.ENTER)
+        content = driver.page_source
+
+        return content
